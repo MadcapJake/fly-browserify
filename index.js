4c687d1c5a0911eed1b2bbe364f41ca5de9ff81a
luke <luke.edwards05@gmail.com>
2016-02-06 19:27:00 -0800

    emit & format plugin_error. does not kill fly runner

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ function compile(compiler, cb) {
   return this.unwrap((files) => {
     files.forEach(file => compiler.add(file))
     compiler.bundle(function (err, buf) {
-      if (err) throw err
+      if (err) {
+        return this.emit("plugin_error", {
+          plugin: "fly-browserify",
+          error: getError(err.message, this.root)
+        })
+      }
+
       cb(null, buf.toString())
     })
   })
+}
+
+function getError(msg, basedir) {
+  return msg.replace(RegExp(basedir, "g"), "")
+    .replace(": ", ": \n\n  ")
+    .replace(" while parsing", "\n\nwhile parsing") + "\n"
 }
 
 module.exports = function() {
