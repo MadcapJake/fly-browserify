13b1cbc220f966ca7948da939f03d30fe8a4fd26
luke <luke.edwards05@gmail.com>
2016-02-06 19:21:31 -0800

    extract function, no internal dependents

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const browserify = require('browserify')
+const browserify = require("browserify")
 
-module.exports = function () {
-  const compile = (compiler, cb) => {
-    return this.unwrap((files) => {
-      files.forEach(file => compiler.add(file))
-      compiler.bundle(function (err, buf) {
-        if (err) throw err
-        cb(null, buf.toString())
-      })
+function compile(compiler, cb) {
+  return this.unwrap((files) => {
+    files.forEach(file => compiler.add(file))
+    compiler.bundle(function (err, buf) {
+      if (err) throw err
+      cb(null, buf.toString())
     })
-  }
-  this.filter('browserify', (source, options) => {
+  })
+}
+
+module.exports = function() {
+  this.filter("browserify", (source, options) => {
     const b = browserify(options)
     return this.defer(compile.bind(this))(b)
   })
