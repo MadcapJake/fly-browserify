b8b2898ce05130f8db5741ca0d1adc13d5da9444
Luke Edwards <luke.edwards05@gmail.com>
2016-11-30 12:51:01 -0800

    add transform tests

--- test/index.js
+++ test/index.js
@@ -8,7 +8,7 @@ const dir = join(__dirname, 'fixtures');
 const tmp = join(__dirname, 'tmp');
 
 test('fly-browserify', t => {
-	t.plan(11);
+	t.plan(13);
 
 	const fly = new Fly({
 		plugins: [{
@@ -27,7 +27,7 @@ test('fly-browserify', t => {
 				yield this.source(all).browserify().target(tmp);
 				const arr1 = yield open();
 				const str1 = yield read('foo.js');
-				t.equal(arr1.length, 3, 'loop thru all `source` files by default');
+				t.equal(arr1.length, 4, 'loop thru all `source` files by default');
 				t.true(str1.indexOf(chk) !== -1, 'bundle each file by default');
 				yield this.clear(tmp);
 
@@ -59,6 +59,23 @@ test('fly-browserify', t => {
 				const str5 = yield read('foo.js');
 				t.equal(arr5.length, 2, 'prefer `opts.entries` over `source` value; multiple (array)');
 				t.true(str5.indexOf(chk) !== -1, 'bundle each file');
+				yield this.clear(tmp);
+
+				const opt = {presets: ['es2015']};
+				const es5 = `var obj = { hello: 'world' };`;
+
+				yield this.source(`${dir}/sub/baz.js`).browserify({
+					transform: [require('babelify').configure(opt)]
+				}).target(tmp);
+				const str6 = yield read('baz.js');
+				t.true(str6.indexOf(es5) !== -1, 'apply `opts.transform` using `require()`');
+				yield this.clear(tmp);
+
+				yield this.source(`${dir}/sub/baz.js`).browserify({
+					transform: [['babelify', opt]]
+				}).target(tmp);
+				const str7 = yield read('baz.js');
+				t.true(str7.indexOf(es5) !== -1, 'apply `opts.transform` using `array`');
 				yield this.clear(tmp);
 			}
 		}
